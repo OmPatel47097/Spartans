9ef637854d5723ee60db4c75dac99967ca2f33b9
unknown <mistrydev6@gmail.com>
2022-03-27 02:56:52 +0530

    Web Done

--- HTML/Script_Demo.js
+++
@@ -1,7 +0,0 @@
-let tp = document.querySelector('button');
-tp.addEventListener('click', inputMsg);
-
-function inputMsg() {
-    let name = prompt('Enter the name of Student');
-    tp.textContent = 'Enter your ID to Enter:' + name;
-}
